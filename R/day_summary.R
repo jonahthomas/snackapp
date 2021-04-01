15986c074fa7aae658b7e9f2cbd3d89c45e24ad4
Jonah Thomas <j.j.c.thomas@lboro.ac.uk>
2021-04-01 11:36:19 +0100

    update functions to accept dataframe or list of data frames

--- R/day_summary.R
+++ R/day_summary.R
@@ -3,8 +3,8 @@
 #' Summarising the usage data for a participant on each day they used the intervention. A single tibble is
 #' generated.
 #'
-#' @param folder_path A path to the folder which contains the SnackApp usage data. The file should only contain csv files.
-#' Defaults to a folder in the current work directory named "data".
+#' @param data The SnackApp usage data that is to be analysed. This parameter accepts the output of the load_data function.
+#' It also accepts a dataframe with 6 columns: id, Date, Event, Metric, Goal Is Automatic Set and Goal Value Set.
 #' @param csv A logical variable that decides whether a csv output file is created.
 #' @param output_path A path to a folder where the csv summary file(s) will be written. Defaults to a folder in the current
 #' work directory named "summary".
@@ -12,91 +12,80 @@
 #' @return A dataframe that summarises the SnackApp usage data for each day a participant used the device.
 #' @export
 #'
-#' @examples
-#'
-day_summary <- function(folder_path = file.path(getwd(), "data"), csv = FALSE, output_path = file.path(getwd(), "summary")) {
+#' @importFrom rlang .data
 
-  # take file path and extract file names within the folder
+day_summary <- function(data, csv = FALSE, output_path = file.path(getwd(), "summary")) {
 
-  folder_path %>%
-    list.files() %>%
-    .[stringr::str_detect(., "csv")] -> file_names
+  if(inherits(data, "data.frame") == FALSE){
+    data <- data %>%
+      dplyr::bind_rows(.id = "id")
+  }
 
-  # loop through each file name and read the csv in, then manipulate the data, assign this dataframe to df_list
-
-  file_names %>%
-    purrr::map(function(file_names) { # iterate through each file name
-      utils::read.csv(file.path(folder_path, file_names), stringsAsFactors = FALSE) %>%
-        dplyr::mutate(
-          id = as.numeric(substr(file_names, 20, 21)), # use file name as a unique id column for each dataframe
-          date = as.POSIXct(`Date`), # convert date from chr to POSIXct format
-          year = as.numeric(format(date, "%Y")),
-          month = as.numeric(format(date, "%m")), # create month and day column
-          day = as.numeric(format(date, "%d"))
-        ) %>%
-        dplyr::relocate(id, date, month, day) %>%
-        dplyr::select(-`Date`)
-    }) -> df_list # Assign to a list
-
-  # bind the rows of all the dataframes in the list into one dataframe
-
-  dplyr::bind_rows(df_list) -> data
+  data <- data %>%
+    dplyr::mutate(
+      date = as.POSIXct(.data$Date), # convert date from chr to POSIXct format
+      year = as.numeric(format(.data$date, "%Y")),
+      month = as.numeric(format(.data$date, "%m")), # create month and day column
+      day = as.numeric(format(.data$date, "%d"))
+    ) %>%
+    dplyr::relocate(.data$id, .data$date, .data$month, .data$day) %>%
+    dplyr::select(-.data$Date)
 
   # calculate summary date for the total duration of app usage
 
   data %>%
-    dplyr::filter(Event == "app-state-changed") %>%
+    dplyr::filter(.data$Event == "app-state-changed") %>%
     dplyr::mutate(
-      diff = lubridate::as.duration(date - dplyr::lag(date))
+      diff = lubridate::as.duration(.data$date - dplyr::lag(.data$date))
     ) %>%
-    dplyr::filter(Metric == "background") %>%
-    dplyr::group_by(id, year, month, day) %>%
+    dplyr::filter(.data$Metric == "background") %>%
+    dplyr::group_by(.data$id, .data$year, .data$month, .data$day) %>%
     dplyr::summarise(
-      total_time = sum(diff),
-      average_time = mean(diff, na.rm = TRUE),
-      std_dev_time = stats::sd(diff, na.rm = TRUE),
-      minimum_time = min(diff, na.rm = TRUE),
-      maximum_time = max(diff, na.rm = TRUE)
+      total_time = sum(.data$diff),
+      average_time = mean(.data$diff, na.rm = TRUE),
+      std_dev_time = stats::sd(.data$diff, na.rm = TRUE),
+      minimum_time = min(.data$diff, na.rm = TRUE),
+      maximum_time = max(.data$diff, na.rm = TRUE)
     ) -> state_change_summary
 
   # create app element flagging columns and calcualte time difference
 
   data <- data %>%
     dplyr::mutate(
-      lag_date = dplyr::lag(date),
-      my_stat = dplyr::if_else(stringr::str_detect(Metric, "my-stat"), 1, 0),
-      my_stat = dplyr::lag(my_stat, 1),
-      stat_diff = dplyr::if_else(my_stat == 1, as.numeric(date - lag_date), 0),
-      resources = dplyr::if_else(stringr::str_detect(Metric, "resources") | stringr::str_detect(Metric, "your-health") | stringr::str_detect(Metric, "faq") | stringr::str_detect(Metric, "forum") & !stringr::str_detect(Metric, "fitbit"), 1, 0),
-      resources = dplyr::lag(resources, 1),
-      resource_diff = dplyr::if_else(resources == 1, as.numeric(date - lag_date), 0),
-      my_goals = dplyr::if_else(stringr::str_detect(Metric, "my-goal"), 1, 0),
-      my_goals = dplyr::lag(my_goals, 1),
-      goal_diff = dplyr::if_else(my_goals == 1, as.numeric(date - lag_date), 0),
-      notifications = dplyr::if_else(stringr::str_detect(Event, "notification"), 1, 0)
+      lag_date = dplyr::lag(.data$date),
+      my_stat = dplyr::if_else(stringr::str_detect(.data$Metric, "my-stat"), 1, 0),
+      my_stat = dplyr::lag(.data$my_stat, 1),
+      stat_diff = dplyr::if_else(.data$my_stat == 1, as.numeric(.data$date - .data$lag_date), 0),
+      resources = dplyr::if_else(stringr::str_detect(.data$Metric, "resources") | stringr::str_detect(.data$Metric, "your-health") | stringr::str_detect(.data$Metric, "faq") | stringr::str_detect(.data$Metric, "forum") & !stringr::str_detect(.data$Metric, "fitbit"), 1, 0),
+      resources = dplyr::lag(.data$resources, 1),
+      resource_diff = dplyr::if_else(.data$resources == 1, as.numeric(.data$date - .data$lag_date), 0),
+      my_goals = dplyr::if_else(stringr::str_detect(.data$Metric, "my-goal"), 1, 0),
+      my_goals = dplyr::lag(.data$my_goals, 1),
+      goal_diff = dplyr::if_else(.data$my_goals == 1, as.numeric(.data$date - .data$lag_date), 0),
+      notifications = dplyr::if_else(stringr::str_detect(.data$Event, "notification"), 1, 0)
     )
 
   # summarise the usage of difference data for each individual app element
 
   data %>%
-    dplyr::group_by(id, year, month, day) %>%
+    dplyr::group_by(.data$id, .data$year, .data$month, .data$day) %>%
     dplyr::summarise(
-      total_stat = sum(stat_diff, na.rm = TRUE),
-      total_resource = sum(resource_diff, na.rm = TRUE),
-      total_goal = sum(goal_diff, na.rm = TRUE),
-      average_stat = mean(stat_diff, na.rm = TRUE),
-      average_resource = mean(resource_diff, na.rm = TRUE),
-      average_goal = mean(goal_diff, na.rm = TRUE),
-      stdev_goal = stats::sd(stat_diff, na.rm = TRUE),
-      stdev_resources = stats::sd(resource_diff, na.rm = TRUE),
-      stdev_goal = stats::sd(goal_diff, na.rm = TRUE),
-      min_stat = min(stat_diff[which(stat_diff > 0)]),
-      min_resource = min(resource_diff[which(resource_diff > 0)]),
-      min_goal = min(goal_diff[which(goal_diff > 0)]),
-      max_stat = max(stat_diff, na.rm = TRUE),
-      max_resource = max(resource_diff, na.rm = TRUE),
-      max_goal = max(goal_diff, na.rm = TRUE),
-      notificaition_count = sum(notifications)
+      total_stat = sum(.data$stat_diff, na.rm = TRUE),
+      total_resource = sum(.data$resource_diff, na.rm = TRUE),
+      total_goal = sum(.data$goal_diff, na.rm = TRUE),
+      average_stat = mean(.data$stat_diff, na.rm = TRUE),
+      average_resource = mean(.data$resource_diff, na.rm = TRUE),
+      average_goal = mean(.data$goal_diff, na.rm = TRUE),
+      stdev_goal = stats::sd(.data$stat_diff, na.rm = TRUE),
+      stdev_resources = stats::sd(.data$resource_diff, na.rm = TRUE),
+      stdev_goal = stats::sd(.data$goal_diff, na.rm = TRUE),
+      min_stat = min(.data$stat_diff[which(.data$stat_diff > 0)]),
+      min_resource = min(.data$resource_diff[which(.data$resource_diff > 0)]),
+      min_goal = min(.data$goal_diff[which(.data$goal_diff > 0)]),
+      max_stat = max(.data$stat_diff, na.rm = TRUE),
+      max_resource = max(.data$resource_diff, na.rm = TRUE),
+      max_goal = max(.data$goal_diff, na.rm = TRUE),
+      notificaition_count = sum(.data$notifications)
     ) -> summary
 
   # due to minimum not including zero, coerces inf so convert these back to zeros
@@ -106,12 +95,14 @@ day_summary <- function(folder_path = file.path(getwd(), "data"), csv = FALSE, o
 
   # bind dataframes from state change and summary using inner join
 
-  summary <- dplyr::inner_join(state_change_summary, summary, keep = FALSE) %>%
-    round(digits = 2) %>%
+  summary <- dplyr::inner_join(state_change_summary, summary) %>%
     dplyr::mutate(
-      date = lubridate::make_date(year, month, day)
+      date = lubridate::make_date(.data$year, .data$month, .data$day)
     ) %>%
-    dplyr::relocate(id, date, year, month, day)
+    dplyr::relocate(.data$id, .data$date, .data$year, .data$month, .data$day) %>%
+    dplyr::mutate(
+      dplyr::across(where(is.numeric), round)
+    )
 
   # write the total summary data back to a csv
 
--- R/individual_summary.R
+++ R/individual_summary.R
@@ -1,10 +1,10 @@
 #' individual_summary
 #'
 #' Creating a separate dataframe for each participant for which data has been collected. A list of tibbles are generated while
-#' separate .csv files can be produced.
+#' separate .csv files can be produced and written to file.
 #'
-#' @param folder_path A path to the folder which contains the SnackApp usage data. The file should only contain csv files.
-#' Defaults to a folder in the current work directory named "data".
+#' @param data The SnackApp usage data that is to be analysed. This parameter accepts the output of the load_data function.
+#' It also accepts a dataframe with 6 columns: id, Date, Event, Metric, Goal Is Automatic Set and Goal Value Set.
 #' @param csv A logical variable that decides whether a csv output file is created.
 #' @param output_path A path to a folder where the csv summary file(s) will be written. Defaults to a folder in the current
 #' work directory named "summary".
@@ -12,109 +12,113 @@
 #' @return A dataframe is generated to summarise each participants usage data.
 #' @export
 #'
-#' @examples
-#'
-individual_summary <- function(folder_path = file.path(getwd(), "data"), csv = FALSE, output_path = file.path(getwd(), "summary")) {
+#' @importFrom rlang .data
 
-  # take file path and extract file names within the folder
+individual_summary <- function(data, csv = FALSE, output_path = file.path(getwd(), "summary")) {
 
-  folder_path %>%
-    list.files() %>%
-    .[stringr::str_detect(., "csv")] -> file_names
+  if(inherits(data, "data.frame") == FALSE){
+    data <- data %>%
+      dplyr::bind_rows(.id = "id")
+  }
 
-  # loop through each file name and read the csv in, then manipulate the data, assign this dataframe to df_list
+  file_id <- unique(data$id)
 
-  file_names %>%
-    purrr::map(function(file_names) { # iterate through each file name
-      utils::read.csv(file.path(folder_path, file_names), stringsAsFactors = FALSE) %>%
-        dplyr::mutate(
-          id = as.numeric(substr(file_names, 20, 21)), # use file name as a unique id column for each dataframe
-          date = as.POSIXct(`Date`), # convert date from chr to POSIXct format
-          year = as.numeric(format(date, "%Y")),
-          month = as.numeric(format(date, "%m")), # create month and day column
-          day = as.numeric(format(date, "%d"))
-        ) %>%
-        dplyr::relocate(id, date, month, day) %>%
-        dplyr::select(-`Date`) -> data
+  data <- data %>%
+    dplyr::mutate(
+      date = as.POSIXct(.data$Date), # convert date from chr to POSIXct format
+      year = as.numeric(format(.data$date, "%Y")),
+      month = as.numeric(format(.data$date, "%m")), # create month and day column
+      day = as.numeric(format(.data$date, "%d"))
+    ) %>%
+    dplyr::relocate(.data$id, .data$date, .data$month, .data$day) %>%
+    dplyr::select(-.data$Date)
 
-      data <- as.data.frame(data)
+  # calculate summary date for the total duration of app usage
 
-      file_id <- substr(file_names, 20, 21)
+  data %>%
+    dplyr::filter(.data$Event == "app-state-changed") %>%
+    dplyr::mutate(
+      diff = lubridate::as.duration(.data$date - dplyr::lag(.data$date))
+    ) %>%
+    dplyr::filter(.data$Metric == "background") %>%
+    dplyr::group_by(.data$id, .data$year, .data$month, .data$day) %>%
+    dplyr::summarise(
+      total_time = sum(.data$diff),
+      average_time = mean(.data$diff, na.rm = TRUE),
+      std_dev_time = stats::sd(.data$diff, na.rm = TRUE),
+      minimum_time = min(.data$diff, na.rm = TRUE),
+      maximum_time = max(.data$diff, na.rm = TRUE)
+    ) -> state_change_summary
 
-      data %>%
-        dplyr::filter(Event == "app-state-changed") %>%
-        dplyr::mutate(
-          diff = lubridate::as.duration(date - dplyr::lag(date))
-        ) %>%
-        dplyr::filter(Metric == "background") %>%
-        dplyr::group_by(id, year, month, day) %>%
-        dplyr::summarise(
-          total_time = sum(diff),
-          average_time = mean(diff, na.rm = TRUE),
-          std_dev_time = stats::sd(diff, na.rm = TRUE),
-          minimum_time = min(diff, na.rm = TRUE),
-          maximum_time = max(diff, na.rm = TRUE)
-        ) -> state_change_summary
+  # create app element flagging columns and calcualte time difference
 
-      # create app element flagging columns and calcualte time difference
+  data <- data %>%
+    dplyr::mutate(
+      lag_date = dplyr::lag(.data$date),
+      my_stat = dplyr::if_else(stringr::str_detect(.data$Metric, "my-stat"), 1, 0),
+      my_stat = dplyr::lag(.data$my_stat, 1),
+      stat_diff = dplyr::if_else(.data$my_stat == 1, as.numeric(.data$date - .data$lag_date), 0),
+      resources = dplyr::if_else(stringr::str_detect(.data$Metric, "resources") | stringr::str_detect(.data$Metric, "your-health") | stringr::str_detect(.data$Metric, "faq") | stringr::str_detect(.data$Metric, "forum") & !stringr::str_detect(.data$Metric, "fitbit"), 1, 0),
+      resources = dplyr::lag(.data$resources, 1),
+      resource_diff = dplyr::if_else(.data$resources == 1, as.numeric(.data$date - .data$lag_date), 0),
+      my_goals = dplyr::if_else(stringr::str_detect(.data$Metric, "my-goal"), 1, 0),
+      my_goals = dplyr::lag(.data$my_goals, 1),
+      goal_diff = dplyr::if_else(.data$my_goals == 1, as.numeric(.data$date - .data$lag_date), 0),
+      notifications = dplyr::if_else(stringr::str_detect(.data$Event, "notification"), 1, 0)
+    )
 
-      data <- data %>%
-        dplyr::mutate(
-          lag_date = dplyr::lag(date),
-          my_stat = dplyr::if_else(stringr::str_detect(Metric, "my-stat"), 1, 0),
-          my_stat = dplyr::lag(my_stat, 1),
-          stat_diff = dplyr::if_else(my_stat == 1, as.numeric(date - lag_date), 0),
-          resources = dplyr::if_else(stringr::str_detect(Metric, "resources") | stringr::str_detect(Metric, "your-health") | stringr::str_detect(Metric, "faq") | stringr::str_detect(Metric, "forum") & !stringr::str_detect(Metric, "fitbit"), 1, 0),
-          resources = dplyr::lag(resources, 1),
-          resource_diff = dplyr::if_else(resources == 1, as.numeric(date - lag_date), 0),
-          my_goals = dplyr::if_else(stringr::str_detect(Metric, "my-goal"), 1, 0),
-          my_goals = dplyr::lag(my_goals, 1),
-          goal_diff = dplyr::if_else(my_goals == 1, as.numeric(date - lag_date), 0),
-          notifications = dplyr::if_else(stringr::str_detect(Event, "notification"), 1, 0)
-        )
+  # summarise the usage of difference data for each individual app element
 
-      # summarise the usage of difference data for each individual app element
+  data %>%
+    dplyr::group_by(.data$id, .data$year, .data$month, .data$day) %>%
+    dplyr::summarise(
+      total_stat = sum(.data$stat_diff, na.rm = TRUE),
+      total_resource = sum(.data$resource_diff, na.rm = TRUE),
+      total_goal = sum(.data$goal_diff, na.rm = TRUE),
+      average_stat = mean(.data$stat_diff, na.rm = TRUE),
+      average_resource = mean(.data$resource_diff, na.rm = TRUE),
+      average_goal = mean(.data$goal_diff, na.rm = TRUE),
+      stdev_goal = stats::sd(.data$stat_diff, na.rm = TRUE),
+      stdev_resources = stats::sd(.data$resource_diff, na.rm = TRUE),
+      stdev_goal = stats::sd(.data$goal_diff, na.rm = TRUE),
+      min_stat = min(.data$stat_diff[which(.data$stat_diff > 0)]),
+      min_resource = min(.data$resource_diff[which(.data$resource_diff > 0)]),
+      min_goal = min(.data$goal_diff[which(.data$goal_diff > 0)]),
+      max_stat = max(.data$stat_diff, na.rm = TRUE),
+      max_resource = max(.data$resource_diff, na.rm = TRUE),
+      max_goal = max(.data$goal_diff, na.rm = TRUE),
+      notificaition_count = sum(.data$notifications)
+    ) -> summary
 
-      data %>%
-        dplyr::group_by(id, year, month, day) %>%
-        dplyr::summarise(
-          total_stat = sum(stat_diff, na.rm = TRUE),
-          total_resource = sum(resource_diff, na.rm = TRUE),
-          total_goal = sum(goal_diff, na.rm = TRUE),
-          average_stat = mean(stat_diff, na.rm = TRUE),
-          average_resource = mean(resource_diff, na.rm = TRUE),
-          average_goal = mean(goal_diff, na.rm = TRUE),
-          stdev_goal = stats::sd(stat_diff, na.rm = TRUE),
-          stdev_resources = stats::sd(resource_diff, na.rm = TRUE),
-          stdev_goal = stats::sd(goal_diff, na.rm = TRUE),
-          min_stat = min(stat_diff[which(stat_diff > 0)]),
-          min_resource = min(resource_diff[which(resource_diff > 0)]),
-          min_goal = min(goal_diff[which(goal_diff > 0)]),
-          max_stat = max(stat_diff, na.rm = TRUE),
-          max_resource = max(resource_diff, na.rm = TRUE),
-          max_goal = max(goal_diff, na.rm = TRUE),
-          notificaition_count = sum(notifications)
-        ) -> summary
+  # due to minimum not including zero, coerces inf so convert these back to zeros
 
-      # due to minimum not including zero, coerces inf so convert these back to zeros
+  is.na(summary) <- sapply(summary, is.infinite)
+  summary[is.na(summary)] <- 0
 
-      is.na(summary) <- sapply(summary, is.infinite)
-      summary[is.na(summary)] <- 0
+  # bind dataframes from state change and summary using inner join
 
-      # bind dataframes from state change and summary using inner join
+  summary <- dplyr::inner_join(state_change_summary, summary) %>%
+    dplyr::mutate(
+      date = lubridate::make_date(.data$year, .data$month, .data$day)
+    ) %>%
+    dplyr::relocate(.data$id, .data$date, .data$year, .data$month, .data$day) %>%
+    dplyr::mutate(
+      dplyr::across(where(is.numeric), round)
+    )
 
-      summary <- dplyr::inner_join(state_change_summary, summary, keep = FALSE) %>%
-        round(digits = 2) %>%
-        dplyr::mutate(
-          date = lubridate::make_date(year, month, day)
-        ) %>%
-        dplyr::relocate(id, date, year, month, day)
+  summary <- split(summary, f = summary$id)
 
-      # write the total summary data back to a csv
+  # write the total summary data back to a csv
 
-      if (csv == TRUE) {
-        utils::write.csv(summary, file = file.path(output_path, paste(file_id, "_summary.csv", sep = "")))
-      }
-      return(summary)
-    })
+  if (csv == TRUE) {
+
+    for (i in seq_along(summary)) {
+      write.csv(summary[[i]], file.path(output_path, paste0(summary[[i]]$id[1], "_summary.csv")))
+    }
+  }
+
+  return(summary)
 }
+
+
+
